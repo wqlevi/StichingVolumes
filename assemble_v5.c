27f4e20b7b10766c1213e83f4310f7a8fdb18cdb
wqlevi <wq.levi@gmail.com>
2021-10-31 21:30:34 +0100

    corrected 2d

--- assemble_v5.c
+++ assemble_v5.c
@@ -27,58 +27,55 @@ int * make_sample(int size_x, int size_y, _Bool zeros){
 			else
 			{
 				// crop initialization(element = start 1)
-				mat[j*size_x+i] = j*size_x+i+1; 
-				
+				mat[j*size_x+i] = j*size_x+i+1;
+
 			}
 		}
-	}	
+	}
 	return mat;
 }
 
 int main()
 {
-	int write_to_file(int count, int col, int *data, char const *fileName); 
+	int write_to_file(int count, int col, int *data, char const *fileName);
 	int * assemble_2d(int * image, int * crop );
 
 	int *crop,*image;
 	crop = make_sample(M,M,0);// crop
 	image = make_sample(X,Y,1); // image
-	//*************BEGIN
-	int Bound_X=0,Bound_Y=0;
-	if(crop == NULL || image == NULL) exit(1);
-	for(int j=0; j<Y; j+=J) // if only both step sizes are identical, it's easier...
 	image = assemble_2d(image,crop);
 	write_to_file(X*Y,X,image,"example.csv");
 	write_to_file( M*M,M,crop,"example_crop.csv");
-	
+
 	return 0;
 }
 
 /* Writting image to csv*/
-int write_to_file(int count, int col, int *data, char const *fileName) 
-{ 
-  FILE *f = fopen(fileName, "w"); 
-  if (f == NULL) return -1; 
+int write_to_file(int count, int col, int *data, char const *fileName)
+{
+  FILE *f = fopen(fileName, "w");
+  if (f == NULL) return -1;
   int idx = 1;
-	for (int i=0;i<count;i++) { 
+	for (int i=0;i<count;i++) {
 	fprintf(f,"%d",data[i]); // data starts from 0-index
 	if(idx%col == 0) fprintf(f,"\n");
 	else fprintf(f,",");
 	idx++;
-  } 
-  fclose(f); 
-  return 0; 
-} 
+  }
+  fclose(f);
+  return 0;
+}
 
 /* Assemblying function 2D*/
 int * assemble_2d(int * image, int * crop )
-{  
+{
 	int Bound_X=0,Bound_Y=0;
 	if(crop == NULL || image == NULL) exit(1);
+	for(int j=0; j<Y; j+=J) {
 	for (int i=0; i<X; i+=I)
 		{
 			if((i<128)&&(j<128)) continue; // 128*128 initial
-			else if((i<128)^(j<128))	   // either one of axis < 128 
+			else if((i<128)^(j<128))	   // either one of axis < 128
 			{
 				if(i<128) Bound_X = RES_X; // on Y axis
 				if(j<128) Bound_Y =	RES_Y; // on X axis
@@ -89,18 +86,12 @@ int * assemble_2d(int * image, int * crop )
 			{
 				for(int p=0;p<I+Bound_X;p++) // x_step
 				{
-					
-					image[(q+j)*X+i+p] = crop[M*((RES_Y-Bound_Y)+q)+(RES_X-Bound_X)+p+1];
-					
-					
-					
-				
+					image[(q+j)*X+i+p] = crop[M*((RES_Y-Bound_Y)+q)+(RES_X-Bound_X)+p];
 				}
 			}
-			
-			
-		}
-	}	
-	return image;
-}         
 
+
+		}
+	}
+	return image;
+}
